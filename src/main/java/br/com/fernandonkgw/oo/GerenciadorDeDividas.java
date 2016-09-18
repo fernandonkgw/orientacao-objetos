febb93328d99e4b375be4a4a237cd1fa3aba62ed
fernandonkgw <fernandonkgw@gmail.com>
2016-09-18 09:11:50 -0300

    Criação das classes módulo financeiro

---
+++ src/main/java/br/com/fernandonkgw/oo/GerenciadorDeDividas.java
@@ -0,0 +1,12 @@
+package br.com.fernandonkgw.oo;
+
+public class GerenciadorDeDividas {
+
+	public void efetuaPagamento(Divida divida, String nomePagador, String cnpjPagador, double valor) {
+		Pagamento pagamento = new Pagamento();
+		pagamento.setCnpjPagador(cnpjPagador);
+		pagamento.setPagador(nomePagador);
+		pagamento.setValor(valor);
+		divida.registra(pagamento);
+	}
+}
